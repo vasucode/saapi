c684b6a82123e2705219ff869a9eaf16be0c060f
Code Matter <code@srinivasuraju.com>
2017-11-03 10:50:05 -0500

    initial commit

---
+++ db/migrate/20171103150317_devise_create_users.rb
@@ -0,0 +1,56 @@
+class DeviseCreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
+
+      ## User Profile
+      t.string :phone_mobile
+      t.boolean :send_text, :default => false
+      t.string :phone_home
+      t.string :city
+      t.string :state
+      t.string :country
+      t.string :github_acnt
+      t.string :twitter_acnt
+      t.string :facebook_acnt
+      t.string :security_ques1
+      t.string :security_ans1
+      t.string :security_ques2
+      t.string :security_ans2
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+end
